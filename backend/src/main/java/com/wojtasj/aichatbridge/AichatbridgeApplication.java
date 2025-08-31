f92f2112e895f318f1c0ff1f6b4d924652cb7cb8
kwojtas912@gmail.com <kwojtas912@gmail.com>
2025-08-31 20:12:34 +0200

    Init commit with configuration and implementation for docker (docker-compose, Dockerfile), flyway, postgresql via Docker and .env files. Separated into production and development configurations.

---
+++ backend/src/main/java/com/wojtasj/aichatbridge/AichatbridgeApplication.java
@@ -0,0 +1,13 @@
+package com.wojtasj.aichatbridge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AichatbridgeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AichatbridgeApplication.class, args);
+	}
+
+}
